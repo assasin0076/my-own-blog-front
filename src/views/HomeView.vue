6df39d5b1f24fb02c5b6fea8561ebb0aae084474
Victor <viktor.generalov@nlogic.ai>
2022-10-15 10:36:16 +0400

    feat: добавлена панель навигации

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { nanoid } from "nanoid";
+
+const navs = [
+  {
+    to: "/",
+    label: "Главная",
+    id: nanoid(),
+  },
+  {
+    to: "#",
+    label: "Не главная",
+    id: nanoid(),
+  },
+];
+</script>
 
 <template>
-  <header class="nav-header">
-    <a href="#">Главная</a>
-    <a href="#">Другая страница</a>
+  <header class="w-full flex items-center justify-center p-8">
+    <router-link
+      v-for="nav in navs"
+      :key="nav.id"
+      :to="nav.to"
+      class="mr-2 last:mr-0"
+      >{{ nav.label }}</router-link
+    >
   </header>
 </template>
 
