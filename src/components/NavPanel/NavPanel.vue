46cd8979d028fcceb14fbbbfc6b3e73b28eeac35
Victor <viktor.generalov@nlogic.ai>
2022-10-17 02:11:02 +0400

    feat: добавлена секция контактов

--- src/components/NavPanel/NavPanel.vue
+++ src/components/NavPanel/NavPanel.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { nanoid } from "nanoid";
+import TelegramIcon from "@/components/Icons/TelegramIcon.vue";
 
 const navs = [
   {
@@ -16,13 +17,24 @@ const navs = [
 </script>
 
 <template>
-  <header class="w-full flex items-center justify-center p-8">
-    <router-link
-      v-for="nav in navs"
-      :key="nav.id"
-      :to="nav.to"
-      class="mr-2 last:mr-0 hover:text-gray-700 transition-colors"
-      >{{ nav.label }}</router-link
-    >
+  <header class="w-full p-8">
+    <div class="flex w-full justify-center items-center relative">
+      <div class="flex">
+        <router-link
+          v-for="nav in navs"
+          :key="nav.id"
+          :to="nav.to"
+          class="mr-2 last:mr-0 hover:text-gray-700 transition-colors"
+          >{{ nav.label }}</router-link
+        >
+      </div>
+      <div class="flex absolute right-0">
+        <a
+          class="flex items-center hover:text-gray-700 transition-colors"
+          href="https://telegram.me/KawSamurai"
+          ><TelegramIcon class="h-7 w-7" />@KawSamurai</a
+        >
+      </div>
+    </div>
   </header>
 </template>
