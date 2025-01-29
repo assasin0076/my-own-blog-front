89fd0a2ff10a762af8b491ff420d9004838542bf
CUMBOOY <buravlev.1998@mail.ru>
2025-01-30 01:22:42 +0400

    feat: Добавил две кнопки и иконку

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import WorkPlaces from "@/componennts/WorkPlaces/WorkPlaces.vue";
+import WorkPlaces from "@/components/WorkPlaces/WorkPlaces.vue";
 </script>
 
 <template>
